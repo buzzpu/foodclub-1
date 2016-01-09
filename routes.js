7c5deff0541cc19feb893a9f6232255d88a26834
Jollen Chen <jollen@moko365.com>
2016-01-09 10:10:29 +0800

    define REST APIs: bug fixed

--- routes.js
+++ routes.js
@@ -174,13 +174,13 @@ exports = module.exports = function(app, passport) {
   app.get('/account/settings/tumblr/disconnect/', require('./views/account/settings/index').disconnectTumblr);
 
   /* REST APIs */
-  app.get('/1/post', require('./view/api/post').readPost);
-  app.get('/1/post/:id', require('./view/api/post').readPostById);
-  app.get('/1/post/subject/:subject', require('./view/api/post').readPostBySubject);
+  app.get('/1/post', require('./views/api/post').readPost);
+  app.get('/1/post/:id', require('./views/api/post').readPostById);
+  app.get('/1/post/subject/:subject', require('./views/api/post').readPostBySubject);
   app.post('/1/post', require('./views/api/post').createPost);
-  app.put('/1/post', require('./view/api/post').updatePost);
-  app.put('/1/post/:subject/publish', require('./view/api/post').publish);
-  app.put('/1/post/:subject/unpublish', require('./view/api/post').unpublish);
+  app.put('/1/post', require('./views/api/post').updatePost);
+  app.put('/1/post/:subject/publish', require('./views/api/post').publish);
+  app.put('/1/post/:subject/unpublish', require('./views/api/post').unpublish);
 
   //route not found
   app.all('*', require('./views/http/index').http404);
